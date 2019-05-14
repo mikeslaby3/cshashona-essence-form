2e2ba26d4a16b7b07716e893a0653aedd85fbe8c
Mike Slaby <mikeslaby3@gmail.com>
2019-05-14 16:08:24 -0700

    Function for pulling values from birthday form and converting data into correlating essence

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,69 @@ dateArray = [
     new EssenceMap('January 01, 1901 00:00:00', 'December 31, 1901 00:00:00', 'wood'),
     new EssenceMap('January 01, 1900 00:00:00', 'December 31, 1900 00:00:00', 'metal'),
 ]
+
+// month is m
+// Converting into a date object allows for date comparison
+
+function getEssence() {
+    // const MONTH = $('form-month').val();
+    // const DAY = $('form-day').val();
+    // const YEAR = $('form-year').val();
+    const MONTH = 'April';
+    const DAY = 50;
+    const YEAR = 2000;
+  
+    switch(MONTH) {
+        case 'January':
+            m = 1;
+            break;
+        case 'February':
+            m = 2;
+            break;
+        case 'March':
+            m = 3;
+            break;
+        case 'April':
+            m = 4;
+            break;
+        case 'May':
+            m = 5;
+            break;
+        case 'June':
+            m = 6;
+            break;
+        case 'July':
+            m = 7;
+            break;
+        case 'August':
+            m = 8;
+            break;
+        case 'September':
+            m = 9;
+            break;
+        case 'October':
+            m = 10;
+            break;
+        case 'November':
+            m = 11;
+            break;
+        case 'December':
+            m = 12;
+            break;
+        default:
+            m = 1;
+    }
+  
+    let birthDate = new Date(YEAR, m, DAY, 0, 0, 0, 0);
+    let essence = 'fire';
+    
+    for (let i = 0; i < dateArray.length; i++) {
+        if (birthDate >= dateArray[i].startDate && birthDate <= dateArray[i].endDate) {
+            essence = dateArray[i].essence
+        }
+    } 
+    
+    return essence;
+  }
+  
+  console.log(getEssence());
