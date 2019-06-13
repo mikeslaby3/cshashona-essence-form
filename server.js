0ccc30576dbb13031bb23163c2cd552e180f156a
Mike Slaby <mikeslaby3@gmail.com>
2019-06-13 16:30:22 -0700

    Adjusted relative path of index.html

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
