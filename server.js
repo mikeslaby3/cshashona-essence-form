a34795cbde491c82f00ec474e4aff88e7583ffa6
Mike Slaby <mikeslaby3@gmail.com>
2019-06-13 16:24:36 -0700

    Added html route to index

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
 app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
 });
